e4c3a399a7fc998bf5338bb2eed683f0ceb33871
IsacJr <isac.aniceto@hotmail.com>
2021-10-30 15:28:24 -0300

    Fix recipes images link

--- recipe/src/app/recipe/mock/MockedRecipe.ts
+++ recipe/src/app/recipe/mock/MockedRecipe.ts
@@ -4,12 +4,12 @@ export const MockedRecipe: RecipeModel[] = [
     {
         id: 1, name: 'Donuts', category: 2, categoryLabel: 'Deserts', numberOfPeopleServes: 3, difficulty: 4, difficultyLabel: '4',
         ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'], steps: ['step 1', 'step 2', 'step 3'],
-        image:'https://www.sprinklesomesugar.com/wp-content/uploads/2017/11/IMG_4730-2.jpg'
+        image:'https://images.unsplash.com/photo-1606312618475-79449b044093?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
     },
     {
         id: 2, name: 'Naked Cake', category: 2, categoryLabel: 'Deserts', numberOfPeopleServes: 5, difficulty: 3, difficultyLabel: '3',
         ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'], steps: ['step 1', 'step 2', 'step 3'],
-        image:'https://img.elo7.com.br/product/original/273A6FA/bolo-naked-cake-com-morangos-bolo-naked-cake.jpg'
+        image:'https://images.unsplash.com/photo-1488474339733-16a5dd4ba5e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1153&q=80'
     },
     {
         id: 3, name: 'Strogonoff', category: 4, categoryLabel: 'Pasta', numberOfPeopleServes: 3, difficulty: 2, difficultyLabel: '2',
@@ -19,7 +19,7 @@ export const MockedRecipe: RecipeModel[] = [
     {
         id: 4, name: 'Caesar Salad', category: 5, categoryLabel: 'Salad', numberOfPeopleServes: 3, difficulty: 1, difficultyLabel: '1',
         ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'], steps: ['step 1', 'step 2', 'step 3'],
-        image:'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/b89f8de9-0f93-4976-b318-9ab04db353bc/Derivates/d3a08a3c-abb2-452e-9121-168f67c992c8.jpg'
+        image:'https://images.unsplash.com/photo-1607532941433-304659e8198a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80'
     },
     {
         id: 5, name: 'Spaghetti and Meatballs', category: 4, categoryLabel: 'Pasta', numberOfPeopleServes: 5, difficulty: 3, difficultyLabel: '3',
@@ -29,21 +29,21 @@ export const MockedRecipe: RecipeModel[] = [
     {
         id: 6, name: 'Orange Juice', category: 3, categoryLabel: 'Drinks', numberOfPeopleServes: 2, difficulty: 1, difficultyLabel: '1',
         ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'], steps: ['step 1', 'step 2', 'step 3'],
-        image:'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/orange-juice-1296x728-feature.jpg?w=1155&h=1528'
+        image:'https://images.unsplash.com/photo-1601711802544-b6647f0aa6f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
     },
     {
         id: 7, name: 'Ice Cream Sandwich', category: 2, categoryLabel: 'Deserts', numberOfPeopleServes: 3, difficulty: 4, difficultyLabel: '4',
         ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'], steps: ['step 1', 'step 2', 'step 3'],
-        image:'https://media4.s-nbcnews.com/i/newscms/2019_25/1450151/elizabeth-heiskell-today-brownie-ice-cream-sandwiches-today-main-190618_6718a00cba0e9e7c853fc2c9900707e8.jpg'
+        image:'https://images.unsplash.com/photo-1629731410452-79919cf59062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
     },
     {
         id: 8, name: 'Grilled Steak', category: 1, categoryLabel: 'Meat', numberOfPeopleServes: 4, difficulty: 3, difficultyLabel: '3',
         ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'], steps: ['step 1', 'step 2', 'step 3'],
-        image:'https://www.thedailymeal.com/sites/default/files/2015/09/08/mainshutterstock_grilledste.jpg'
+        image:'https://images.unsplash.com/photo-1546964124-0cce460f38ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
     },
     {
         id: 9, name: 'Lasagna Bolognese', category: 4, categoryLabel: 'Pasta', numberOfPeopleServes: 5, difficulty: 2, difficultyLabel: '2',
         ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'], steps: ['step 1', 'step 2', 'step 3'],
-        image:'https://www.365daysofbakingandmore.com/wp-content/uploads/2011/02/Lasagna-FEATURE.jpg'
+        image:'https://images.unsplash.com/photo-1612526079749-4383a1dc9bdf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
     }
 ];
